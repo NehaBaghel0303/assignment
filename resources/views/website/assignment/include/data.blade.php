2d363f5567130db911bd4964462059accd2ab429
Neha Baghel <nehabaghel0303@gmail.com>
2022-12-05 00:45:01 +0530

    clean folders

--- resources/views/website/assignment/include/data.blade.php
+++
@@ -1,9 +0,0 @@
-@foreach ($records  as $key => $record)
-{{-- @dd($record) --}}
-    {{-- @php
-        $items = json_decode($record['data'],true)
-    @endphp --}}
-    {{-- @foreach ($items as $item)   --}}
-    <li>{{ $record->title}}</li> 
-    {{-- @endforeach --}}
-@endforeach
